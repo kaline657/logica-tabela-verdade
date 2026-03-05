23a83d8d47c56404712063efd2f71db84c7e52b5
Kaline Carvalho <carvalho.kaline@discente.ufma.br>
2026-03-05 01:28:07 -0300

    primeira versão do programa - leitura da expressão lógica

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Gerador de Tabela Verdade")
+print("Projeto de Lógica e Matemática Discreta - UFMA")
+
+expressao = input("Digite uma expressão lógica (ex: p ∧ q): ")
+
+print("Expressão digitada:", expressao)
