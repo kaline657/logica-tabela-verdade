38990a6986619c9a033ea1cda881d227d5073e03
Kaline Carvalho <carvalho.kaline@discente.ufma.br>
2026-03-05 01:30:58 -0300

    implementação da identificação automática das proposições

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import re
+
 print("Gerador de Tabela Verdade")
 print("Projeto de Lógica e Matemática Discreta - UFMA")
 
 expressao = input("Digite uma expressão lógica (ex: p ∧ q): ")
 
+# identificar proposições
+proposicoes = sorted(set(re.findall(r"[a-z]", expressao)))
+
 print("Expressão digitada:", expressao)
+print("Proposições encontradas:", proposicoes)
