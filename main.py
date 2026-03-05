9b5ebbce748df2409f3ef1ea7b43da7c45a1b9ce
Kaline Carvalho <carvalho.kaline@discente.ufma.br>
2026-03-05 01:34:14 -0300

    mapeamento de conectivos logicos

--- main.py
+++ main.py
@@ -8,5 +8,22 @@ expressao = input("Digite uma expressão lógica (ex: p ∧ q): ")
 # identificar proposições
 proposicoes = sorted(set(re.findall(r"[a-z]", expressao)))
 
-print("Expressão digitada:", expressao)
+# conectivos aceitos
+conectivos = {
+    "¬": "NAO",
+    "∧": "E",
+    "∨": "OU",
+    "→": "IMPLICA",
+    "↔": "BICONDICIONAL"
+}
+
+# identificar conectivos usados
+conectivos_encontrados = []
+
+for simbolo in conectivos:
+    if simbolo in expressao:
+        conectivos_encontrados.append(simbolo)
+
+print("\nExpressão digitada:", expressao)
 print("Proposições encontradas:", proposicoes)
+print("Conectivos encontrados:", conectivos_encontrados)
